22e63767da048670fc38707145b38d746ec324f8
Zebulun Arendsee <arendsee@iastate.edu>
2020-12-03 14:51:30 -0600

    Add golden test for aadiff with annotations

--- test-data/test.sh
+++ test-data/test.sh
@@ -4,3 +4,6 @@ set -e
 set -u
 
 diff <(flutile aadiff --subtype=H1 h1-aadiff-ex.faa) h1-aadiff-ex.txt
+
+flutile aadiff --caton82 --annotation-tables=rbs.txt --subtype=H1 --join-annotations 1B.2.1-aadiff-test.faa > .obs-aadiff
+diff .obs-aadiff .exp-aadiff
