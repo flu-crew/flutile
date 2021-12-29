9becc51156b86f11fca564245d805a5a6be2c4b3
Zebulun Arendsee <zbwrnz@gmail.com>
2021-12-29 15:19:38 -0600

    Add __all__ to __ini__.py

--- flutile/__init__.py
+++ flutile/__init__.py
@@ -1,2 +1,4 @@
 from flutile.functions import aadiff_table, represent, write_bounds
 from flutile.motifs import get_ha_subtype_nterm_motif
+
+__all__ = ["aadiff_table", "represent", "write_bounds", "get_ha_subtype_nterm_motif"]
